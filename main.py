630d0646e4f8bdb38ab9d5f251b73f780effd94d
rahulsinghrdj <75153249+rahulsinghrdj@users.noreply.github.com>
2021-10-31 18:44:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,29 @@ def add_my_values():
 
     else:
         print('leave it! ')
+        
+ def substract_my_values():
+    make = False
+    x = box.get()
+    for i in x:
+        if '-' in i:
+            make = True
+    if make:
+        print('alright')
+        a = ''
+        for i in x:   
+            if x == int():
+                a -= x
+        print(a)
+
+    else:
+        print('leave it! ')
 
 
 box  = Entry(root, width="50" , font = ('calibri',40,'bold'))
 box.pack()
 clear_screen = Button(root , text = 'C', font = ('calibri',25,'bold') , command = add_my_values)
+Button(root , text = 'substract', font = ('calibri',25,'bold') , command = substract_my_values).place(x=10, y=200)
 clear_screen.place(x=10,y = 100)
 
 
